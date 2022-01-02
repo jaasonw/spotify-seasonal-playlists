885adb99b84f4a16bd6e4751adfaa573dbe50c16
jaasonw <jayywong92@gmail.com>
2022-01-02 00:12:14 -0800

    refactor connection closes to with statements

--- src/database.py
+++ src/database.py
@@ -1,99 +1,123 @@
 import sqlite3
 import os
 
-from constant import CACHE_PATH, DATABASE_NAME
+from constant import DATABASE_NAME
 # utility functions for manipulating the database
 # aka abstracting away all the sql
 
+
+def database_connection():
+    return sqlite3.connect(DATABASE_NAME)
+
+
 def get_user(id):
-    conn = sqlite3.connect(DATABASE_NAME)
-    cursor = conn.cursor()
-    sql = f'SELECT * FROM Users WHERE id="{id}"'
-    cursor.execute(sql)
-    row = cursor.fetchone()
-    conn.commit()
-    conn.close()
-    if row:
-        return row[0]
-    else:
-        return None
+    with database_connection() as conn:
+        cursor = conn.cursor()
+        sql = 'SELECT * FROM Users WHERE id=?'
+        cursor.execute(sql, (id,))
+        row = cursor.fetchone()
+        conn.commit()
+        if row:
+            return row[0]
+        else:
+            return None
+
+
+def get_users():
+    with database_connection() as conn:
+        cursor = conn.cursor()
+        sql = f'SELECT id FROM Users'
+        cursor.execute(sql)
+        row = cursor.fetchall()
+        conn.commit()
+        if row:
+            return row[0]
+        else:
+            return None
+
 
 def update_user(id, field, value):
-    conn = sqlite3.connect(DATABASE_NAME)
-    cursor = conn.cursor()
-    
-    # really scuffed, dynamic type abuse
-    if type(value) == str:
-        sql = f'UPDATE Users SET {field}="{value}" WHERE id="{id}"'
-    else:
-        sql = f'UPDATE Users SET {field}={value} WHERE id="{id}"'
-    cursor.execute(sql)
-    conn.commit()
-    conn.close()
+    with database_connection() as conn:
+        conn = sqlite3.connect(DATABASE_NAME)
+        cursor = conn.cursor()
+
+        # really scuffed, dynamic type abuse
+        if type(value) == str:
+            sql = f'UPDATE Users SET {field}="{value}" WHERE id=?'
+        else:
+            sql = f'UPDATE Users SET {field}={value} WHERE id=?'
+        cursor.execute(sql, (id,))
+        conn.commit()
+
 
 def get_field(id, field):
-    conn = sqlite3.connect(DATABASE_NAME)
-    cursor = conn.cursor()
-    sql = f'SELECT {field} FROM Users WHERE id = "{id}"'
-    cursor.execute(sql)
-    entry = cursor.fetchone()[0]
-    update_user(id, field, entry + 1)
-    conn.close()
-    return entry
+    with database_connection() as conn:
+        conn = sqlite3.connect(DATABASE_NAME)
+        conn.row_factory = lambda c, r: dict(
+            [(col[0], r[idx]) for idx, col in enumerate(c.description)])
+        cursor = conn.cursor()
+        sql = f'SELECT * FROM Users WHERE id = ?'
+        cursor.execute(sql, (id,))
+        entry = cursor.fetchone()[f'{field}']
+        return entry
+
 
 def increment_field(id, field):
     entry = get_field(id, field)
+    print(entry)
     update_user(id, field, entry + 1)
 
+
 def add_user(id):
-    conn = sqlite3.connect(DATABASE_NAME)
-    cursor = conn.cursor()
-    # reset user if they reregister
-    if get_user(id) != None:
-        update_user(id, "error_count", 0)
-    else:
-        sql = f'INSERT INTO Users(id) VALUES(?)'
-        conn.execute(sql, (id,0,0,))
-        conn.commit()
-    conn.close()
+    with database_connection() as conn:
+        cursor = conn.cursor()
+        # reset user if they reregister
+        if get_user(id) != None:
+            update_user(id, "error_count", 0)
+        else:
+            sql = f'INSERT INTO Users(id) VALUES(?)'
+            cursor.execute(sql, (id,))
+            cursor.commit()
+
+
 def add_error(id, error):
-    conn = sqlite3.connect(DATABASE_NAME)
-    sql = f'INSERT INTO Errors(id, error) VALUES(?, ?)'
-    conn.execute(sql, (id, error))
-    conn.commit()
-    conn.close()
+    with database_connection() as conn:
+        sql = f'INSERT INTO Errors(id, error) VALUES(?, ?)'
+        conn.execute(sql, (id, error))
+        conn.commit()
+
 
 def remove_user(id):
-    conn = sqlite3.connect(DATABASE_NAME)
-    cursor = conn.cursor()
-    sql = f'DELETE FROM Users WHERE id = "{id}"'
-    conn.execute(sql)
-    conn.commit()
-    conn.close()
+    with database_connection() as conn:
+        sql = f'DELETE FROM Users WHERE id=?'
+        conn.execute(sql, (id,))
+        conn.commit()
 
-def init_database():
-    conn = sqlite3.connect(DATABASE_NAME)
-    cursor = conn.cursor()
-    cursor.execute(''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='Users' ''')
-    if cursor.fetchone()[0] == 0:
-        conn.execute(''' CREATE TABLE Users(
-            id TEXT,
-            update_count INTEGER DEFAULT 0,
-            error_count INTEGER DEFAULT 0,
-            last_error TEXT DEFAULT "",
-            last_playlist TEXT DEFAULT "",
-            last_update TEXT DEFAULT ""
-        ) ''')
-        
-        print("Created table")
-        for filename in os.listdir(CACHE_PATH):
-            id = filename[len(".cache-"):]
-            add_user(id)
 
-    conn.execute(''' CREATE TABLE IF NOT EXISTS Errors (
-        id text
-            constraint Errors_Users_id_fk
-                references Users (id),
-        error text
-    )''')
-    conn.close()
+# def init_database():
+#     conn = sqlite3.connect(DATABASE_NAME)
+#     cursor = conn.cursor()
+#     cursor.execute(
+#         ''' SELECT count(name) FROM sqlite_master WHERE type='table' AND name='Users' ''')
+#     if cursor.fetchone()[0] == 0:
+#         conn.execute(''' CREATE TABLE Users(
+#             id TEXT,
+#             update_count INTEGER DEFAULT 0,
+#             error_count INTEGER DEFAULT 0,
+#             last_error TEXT DEFAULT "",
+#             last_playlist TEXT DEFAULT "",
+#             last_update TEXT DEFAULT ""
+#         ) ''')
+
+#         print("Created table")
+#         for filename in os.listdir(CACHE_PATH):
+#             id = filename[len(".cache-"):]
+#             add_user(id)
+
+#     conn.execute(''' CREATE TABLE IF NOT EXISTS Errors (
+#         id text
+#             constraint Errors_Users_id_fk
+#                 references Users (id),
+#         error text
+#     )''')
+#     conn.close()
