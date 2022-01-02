d25f1cf9b39fb29e66d0349f8720c435eee7017d
jaasonw <jayywong92@gmail.com>
2022-01-02 00:37:40 -0800

    fix get_users

--- src/database.py
+++ src/database.py
@@ -31,7 +31,7 @@ def get_users():
         row = cursor.fetchall()
         conn.commit()
         if row:
-            return row[0]
+            return [x[0] for x in row]
         else:
             return None
 
