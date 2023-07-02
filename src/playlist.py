20148c4a63bf1f6be51acd22031ab855a12068c1
jaasonw <jayywong92@gmail.com>
2023-07-01 19:46:10 -0700

    fix datetime again

--- src/playlist.py
+++ src/playlist.py
@@ -130,7 +130,7 @@ def update_playlist(client: spotipy.Spotify, user):
     # in utc
     # last_updated = get_newest_date_in_playlist(target_playlist, client)
     last_updated = (
-        dt.strptime(user["last_update"]).replace(tzinfo=tz.utc)
+        dt.strptime(user["last_update"], "%Y-%m-%d %H:%M:%S").replace(tzinfo=tz.utc)
         if user["last_update"] != ""
         else start_season_time(dt.now(tz=tz.utc))
     )
